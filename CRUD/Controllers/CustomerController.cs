779fd878afc0e033504b5331bd4d169d1f6b1ff6
Михаил Крючков <Misha28.95@mail.ru>
2024-03-10 23:13:48 +0300

    CreateCustomer

--- CRUD/Controllers/CustomerController.cs
+++ CRUD/Controllers/CustomerController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CRUD.Controllers
 {
-    public class CustomerController : Controller
+    public class CustomerController(ApplicationDbContext db) : Controller
     {
+        private readonly ApplicationDbContext _db = db;
+
         public IActionResult Index()
         {
+            List<Customer> customers = _db.Customers.ToList();
+            return View(customers);
+        }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (_db.Customers.Any(x => x.Email!.Equals(customer.Email)))
+            {
+                ModelState.AddModelError("email","Такой электронный адрес уже используется");
+            };
+
+            
+            if(ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- CRUD/Models/Customer.cs
+++ CRUD/Models/Customer.cs
@@ -1,10 +1,20 @@
-﻿namespace CRUD.Models
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Поле \"Имя\" обязательно к заполнению!"), MinLength(2, ErrorMessage = "Имя не может быть меньше двух символов")]
+        [DisplayName("Имя")]
         public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        [Required( ErrorMessage = "Поле \"Фамилия\" обязательно к заполнению!"), MinLength(2, ErrorMessage = "Имя не может быть меньше двух символов")]
+        [DisplayName("Фамилия")]
+        public string? LastName { get; set;}
+        [Required(ErrorMessage = "Поле \"Электронная почта\" обязательно к заполнению!"), EmailAddress]
+        [DisplayName("Электронная почта")]
         public string? Email { get; set; }
     }
 }
