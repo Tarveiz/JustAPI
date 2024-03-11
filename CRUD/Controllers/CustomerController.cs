3dcd79978dce4dcf392aac8e1ad3de4dc1d6635e
Михаил Крючков <Misha28.95@mail.ru>
2024-03-11 18:24:17 +0300

    Edit&Delete

--- CRUD/Controllers/CustomerController.cs
+++ CRUD/Controllers/CustomerController.cs
@@ -32,9 +32,52 @@ namespace CRUD.Controllers
             {
                 _db.Customers.Add(customer);
                 _db.SaveChanges();
+                TempData["Success"] = "Пользователь успешно создан";
                 return RedirectToAction("Index");
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            Customer? customer = _db.Customers.Find(id);
+            if (customer == null || id == 0 || id == null) {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            if (_db.Customers.Any(x => x.Email!.Equals(customer.Email) && x.Id != customer.Id))
+            {
+                ModelState.AddModelError("email","Такой электронный адрес уже используется");
+            };
+
+            
+            if(ModelState.IsValid)
+            {
+                _db.Customers.Update(customer);
+                _db.SaveChanges();
+                TempData["Success"] = "Пользователь успешно изменен";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Customer? customer = _db.Customers.Find(id);
+            if (customer == null || id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            TempData["Success"] = "Пользователь успешно удален";
+            return RedirectToAction("Index");
+        }
+
     }
 }
